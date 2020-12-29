5126bb16d92ab520bfaadb21b0bc5607bc7cff96
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-29 12:10:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "openmm_customtools",
-    packages = find_packages(),
+    packages = ['openmm_customtools'],
     version = "0.1.0",
     author = "Xinyan Wang",
     description = "Implementations of enhanced sampling toolkits based on OpenMM CustomIntegrator and CustomBond classes."
